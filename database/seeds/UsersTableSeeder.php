8bfd648ff171e34d346539157d2bfc480832e1f2
wangdong <wangdong@wangdong.com>
2019-12-04 13:17:04 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'wd-513@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
